b6d1d77e5c9ce01a9d888780fa2d53c5ebefdd11
walter_zero <249506369@qq.com>
2023-07-30 11:54:54 +0800

    build报错，按照gpt方式尝试修复

--- app/api/common.ts
+++ app/api/common.ts
@@ -40,7 +40,6 @@ export async function requestOpenai(req: NextRequest) {
         "OpenAI-Organization": process.env.OPENAI_ORG_ID,
       }),
     },
-    duplex: "half",
     cache: "no-store",
     method: req.method,
     body: req.body,
